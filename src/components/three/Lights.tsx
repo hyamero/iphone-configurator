3ada3576995415895dfdb2ccd4bfea79069f7b7a
Dale <daleban.dev@gmail.com>
2022-04-09 21:22:30 +0800

    fix: loader now fills the entire canvas on loading

--- src/components/three/Lights.tsx
+++ src/components/three/Lights.tsx
@@ -5,6 +5,14 @@ export const Lights = () => {
       <directionalLight position={[-10, 0, -15]} intensity={0.2} />
       <directionalLight position={[10, 10, 10]} intensity={0.2} />
       <spotLight position={[0, 10, 0]} intensity={0.7} penumbra={1} />
+      <spotLight
+        position={[5, 10, 5]}
+        angle={0.15}
+        penumbra={1}
+        intensity={10}
+        castShadow
+        shadow-mapSize={[2048, 2048]}
+      />
     </>
   );
 };
